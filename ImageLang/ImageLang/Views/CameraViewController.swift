22cb09c1067dc9d1f8633be0b2d40fc9c839d6ce
Aaron Cleveland <toscleveland@gmail.com>
2020-05-06 02:05:12 -0500

    Switched to Programmatic UI

---
+++ ImageLang/ImageLang/Views/CameraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CameraViewController.swift
+//  ImageLang
+//
+//  Created by Aaron Cleveland on 5/6/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
